fb4b96841793e6cd60935488f0ba5b9999ae4c29
İlker Öztürk <108209673+qorncat@users.noreply.github.com>
2022-07-21 08:29:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 5000
+const PORT = process.env.PORT
 app.set("view engine", 'ejs')
 app.use("/styles",express.static(__dirname + "/styles"))
 
